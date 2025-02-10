efb53532e8c2f792748dc03e43d8b108e5b85a75
Kenet Laptop Ubuntu <kenbok2018@gmail.com>
2025-02-10 10:43:21 -0500

    Updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../css/app.css';
-
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Ruta: assets/app.js
+import '../css/app.scss';
+import 'bootstrap';
